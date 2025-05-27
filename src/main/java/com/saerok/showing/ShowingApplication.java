4cf0e32d8183821516c9f6b658d955e2a5c48d8a
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-05-27 10:17:24 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/com/saerok/showing/ShowingApplication.java
@@ -0,0 +1,13 @@
+package com.saerok.showing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saerok/showing/ShowingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saerok.showing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
