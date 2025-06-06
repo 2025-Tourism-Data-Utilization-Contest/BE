97bc8b45b289a2f8ac216fe06eee310973290276
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-06-06 18:00:09 +0900

    chore: 스웨거 환경 파일 세팅

---
+++ src/main/java/com/saerok/showing/api/global/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.saerok.showing.api.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    servers = {
+        @Server(url = "http://localhost:8081", description = "로컬 서버"),
+        @Server(url = "https://api.showing.site", description = "운영 서버")
+    }
+)
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .components(new Components()
+                .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                    .name("JWT Authentication")
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")
+                    .in(SecurityScheme.In.HEADER)
+                    .description("Access Token을 넣어주세요!")))
+            .info(apiInfo())
+            .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"));
+    }
+
+    private Info apiInfo() {
+        return new Info()
+            .title("Showing REST API Swagger Test Page")
+            .description("Developed by Team Showing")
+            .contact(new Contact()
+                .name("Showing BE GitHub")
+                .url("https://github.com/2025-Tourism-Data-Utilization-Contest/BE"))
+            .version("1.0.0");
+    }
+}
