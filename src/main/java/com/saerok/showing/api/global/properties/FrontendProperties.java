7d25fb91a67ff33d705289d29ec7b7dab663d11f
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-06-14 20:30:25 +0900

    feat: 프론트엔드 홈 url 프로퍼티 클래스 분리

---
+++ src/main/java/com/saerok/showing/api/global/properties/FrontendProperties.java
@@ -0,0 +1,15 @@
+package com.saerok.showing.api.global.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "frontend")
+public class FrontendProperties {
+
+    private String homeUrl;
+}
