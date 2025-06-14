a49a6f6009e1523d1eb59210adda345659e12f7b
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-06-14 20:13:54 +0900

    feat: 이메일로 회원 조회 로직 구현

--- src/main/java/com/saerok/showing/api/domain/member/service/MemberService.java
+++ src/main/java/com/saerok/showing/api/domain/member/service/MemberService.java
@@ -1,6 +1,9 @@
 package com.saerok.showing.api.domain.member.service;
 
+import com.saerok.showing.api.domain.member.entity.Member;
 import com.saerok.showing.api.domain.member.repository.MemberRepository;
+import com.saerok.showing.api.global.exception.ErrorCode;
+import com.saerok.showing.api.global.exception.ShowingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +12,14 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
+    public Member findById(Long memberId) {
+        return memberRepository.findById(memberId)
+            .orElseThrow(() -> ShowingException.from(ErrorCode.MEMBER_NOT_FOUND));
+    }
+
+    public Member findByEmail(String email) {
+        return memberRepository.findByEmail(email)
+            .orElseThrow(() -> ShowingException.from(ErrorCode.MEMBER_NOT_FOUND));
+    }
 }
