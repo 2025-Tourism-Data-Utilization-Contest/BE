33641b35acf9d5adc3a7eee32168d8d51ef2dc63
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-06-14 20:45:35 +0900

    refactor: Role 회원 설명 간소화

--- src/main/java/com/saerok/showing/api/domain/member/entity/Role.java
+++ src/main/java/com/saerok/showing/api/domain/member/entity/Role.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Role {
 
-    USER("ROLE_USER", "일반 회원"),
+    USER("ROLE_USER", "회원"),
     ADMIN("ROLE_ADMIN", "관리자");
 
     private final String key;
