f3828d93a4767e13b7919d904dfffc7bba27bfc8
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-06-15 19:54:25 +0900

    refactor: 생성일자/수정일자 자동 저장 어노테이션 추가

--- src/main/java/com/saerok/showing/ShowingApplication.java
+++ src/main/java/com/saerok/showing/ShowingApplication.java
@@ -2,12 +2,13 @@ package com.saerok.showing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ShowingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShowingApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ShowingApplication.class, args);
+    }
 }
