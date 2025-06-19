89ece273ba65a2e93e217e62246cc5b79bb3abd5
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-06-19 20:25:25 +0900

    feat: 장소 타입 enum 추가

--- src/main/java/com/saerok/showing/api/domain/place/entity/Place.java
+++ src/main/java/com/saerok/showing/api/domain/place/entity/Place.java
@@ -1,12 +1,20 @@
 package com.saerok.showing.api.domain.place.entity;
 
+import com.saerok.showing.api.domain.theme.entity.Theme;
 import com.saerok.showing.api.global.entity.BaseEntity;
+import com.saerok.showing.api.global.utils.Locatable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,7 +29,7 @@ import lombok.Setter;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Table(name = "place")
-public class Place extends BaseEntity {
+public class Place extends BaseEntity implements Locatable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,17 +43,38 @@ public class Place extends BaseEntity {
     private String description;
 
     @Column(name = "price")
-    private Integer price;
+    private int price;
 
     @Column(name = "available_from")
-    private java.time.LocalDate availableFrom;
+    private LocalDate availableFrom;
 
     @Column(name = "available_to")
-    private java.time.LocalDate availableTo;
+    private LocalDate availableTo;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "category")
-    private String category;
+    private PlaceType placeType;
 
     @Column(name = "place_image")
     private String placeImage;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "theme_id")
+    private Theme theme;
+
+    @Column(name = "location_x", nullable = false)
+    private Double locationX;
+
+    @Column(name = "location_y", nullable = false)
+    private Double locationY;
+
+    @Override
+    public double getLocationX() {
+        return this.locationX;
+    }
+
+    @Override
+    public double getLocationY() {
+        return this.locationY;
+    }
 }
