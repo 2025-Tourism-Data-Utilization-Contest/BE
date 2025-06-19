394975fcb6f34047214c77c3ef54751991151578
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-06-19 16:02:55 +0900

    refactor: 새 단건 조회 로직 구현

--- src/main/java/com/saerok/showing/api/domain/bird/controller/BirdController.java
+++ src/main/java/com/saerok/showing/api/domain/bird/controller/BirdController.java
@@ -1,8 +1,14 @@
 package com.saerok.showing.api.domain.bird.controller;
 
+import com.saerok.showing.api.domain.bird.dto.response.BirdDetailResponse;
 import com.saerok.showing.api.domain.bird.service.BirdService;
+import com.saerok.showing.api.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class BirdController {
 
     private final BirdService birdService;
+
+    @Operation(
+        summary = "새 단건 조회",
+        description = "[모든 Role 가능] birdId로 새의 상세 정보를 조회합니다."
+    )
+    @PreAuthorize("hasRole('MEMBER')")
+    @GetMapping("")
+    public ApiResponse<BirdDetailResponse> getBird(@PathVariable Long birdId) {
+        BirdDetailResponse bird = birdService.getBird(birdId);
+        return ApiResponse.success(bird);
+    }
 }
--- src/main/java/com/saerok/showing/api/domain/bird/service/BirdService.java
+++ src/main/java/com/saerok/showing/api/domain/bird/service/BirdService.java
@@ -1,12 +1,33 @@
 package com.saerok.showing.api.domain.bird.service;
 
+import com.saerok.showing.api.domain.bird.dto.response.BirdDetailResponse;
+import com.saerok.showing.api.domain.bird.entity.Bird;
 import com.saerok.showing.api.domain.bird.repository.BirdRepository;
+import com.saerok.showing.api.domain.birdTheme.service.BirdThemeService;
+import com.saerok.showing.api.domain.theme.dto.response.ThemeSummaryResponse;
+import com.saerok.showing.api.global.exception.ErrorCode;
+import com.saerok.showing.api.global.exception.ShowingException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class BirdService {
 
     private final BirdRepository birdRepository;
+    private final BirdThemeService birdThemeService;
+
+    @Transactional(readOnly = true)
+    public BirdDetailResponse getBird(Long birdId){
+        Bird bird = findById(birdId);
+        List<ThemeSummaryResponse> themes = birdThemeService.getThemeNames(birdId);
+        return BirdDetailResponse.toDto(bird, themes);
+    }
+
+    private Bird findById(Long birdId) {
+        return birdRepository.findById(birdId)
+            .orElseThrow(()-> ShowingException.from(ErrorCode.BIRD_NOT_FOUND));
+    }
 }
