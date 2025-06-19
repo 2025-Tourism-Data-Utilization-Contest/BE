81d855d4d4c2fa278ad172d896e676c6caacd42f
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-06-19 20:36:36 +0900

    refactor: 테마 상세 보기 DTO 구현

--- src/main/java/com/saerok/showing/api/domain/theme/dto/response/ThemeDetailResponse.java
+++ src/main/java/com/saerok/showing/api/domain/theme/dto/response/ThemeDetailResponse.java
@@ -1,6 +1,9 @@
 package com.saerok.showing.api.domain.theme.dto.response;
 
 import com.saerok.showing.api.domain.bird.dto.response.BirdSummaryResponse;
+import com.saerok.showing.api.domain.place.dto.response.AttractionPlaceSummaryResponse;
+import com.saerok.showing.api.domain.place.dto.response.ExperiencePlaceSummaryResponse;
+import com.saerok.showing.api.domain.review.dto.response.ReviewSummaryResponse;
 import com.saerok.showing.api.domain.theme.entity.Theme;
 import java.util.List;
 import lombok.Builder;
@@ -20,15 +23,32 @@ public class ThemeDetailResponse {
 
     private List<BirdSummaryResponse> birds;
 
-    // private List<PlaceSummaryResponse> places;
+    private List<AttractionPlaceSummaryResponse> attractionPlaces;
 
-    public static ThemeDetailResponse toDto(Theme theme, List<BirdSummaryResponse> birds) {
+    private List<ExperiencePlaceSummaryResponse> experiencePlaces;
+
+    private int reviewCount;
+
+    private List<ReviewSummaryResponse> reviews;
+
+    public static ThemeDetailResponse toDto(
+        Theme theme,
+        List<BirdSummaryResponse> birds,
+        List<AttractionPlaceSummaryResponse> attractionPlaces,
+        List<ExperiencePlaceSummaryResponse> experiencePlaces,
+        int reviewCount,
+        List<ReviewSummaryResponse> reviews
+    ) {
         return ThemeDetailResponse.builder()
             .id(theme.getId())
             .title(theme.getTitle())
             .description(theme.getDescription())
             .themeImage(theme.getThemeImage())
             .birds(birds)
+            .attractionPlaces(attractionPlaces)
+            .experiencePlaces(experiencePlaces)
+            .reviewCount(reviewCount)
+            .reviews(reviews)
             .build();
     }
 }
