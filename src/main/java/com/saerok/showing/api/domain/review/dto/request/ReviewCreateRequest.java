6063e941e9f259ae1ada2353c2e2cff753c04b1f
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-06-22 19:11:38 +0900

    refactor: 리뷰 생성 DTO 구현

---
+++ src/main/java/com/saerok/showing/api/domain/review/dto/request/ReviewCreateRequest.java
@@ -0,0 +1,40 @@
+package com.saerok.showing.api.domain.review.dto.request;
+
+import com.saerok.showing.api.domain.review.entity.ReviewTargetType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReviewCreateRequest {
+
+    @NotNull
+    @Schema(description = "타겟Id", example = "THEME or PLACE의 pk값을 입력해주세요.(사용자 작성이 아닌 FE에서 처리)")
+    private Long targetId;
+
+    @NotNull
+    @Schema(
+        description = "타겟타입",
+        example = """
+        THEME(테마) or PLACE(장소) 중에 리뷰를 작성할 타입을 골라주세요.
+        사용자 작성이 아닌 FE에서 처리하여 보내줍니다.
+        """
+    )
+    private ReviewTargetType targetType;
+
+    @NotNull
+    @Min(value = 1, message = "별점은 최소 1점 이상이어야 합니다.")
+    @Max(value = 5, message = "별점은 최대 5점까지 가능합니다.")
+    @Schema(description = "리뷰 별점", example = "1~5 중 하나의 정수입니다.")
+    private int rating;
+
+    @NotNull
+    @Size(max = 1000, message = "리뷰 코멘트는 최대 1000자까지 입력할 수 있습니다.")
+    @Schema(description = "리뷰 내용", example = "순천만 습지 처음 와봤는데 너무 재밌었어요~!")
+    private String comment;
+}
