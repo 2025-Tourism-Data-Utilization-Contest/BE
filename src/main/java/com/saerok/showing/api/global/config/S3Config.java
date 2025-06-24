2ba4acd1a5dcc1131a149eaa7856f859a09ba87b
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-06-24 21:11:28 +0900

    refactor: S3 환경변수 주입 경로 리팩토링

--- src/main/java/com/saerok/showing/api/global/config/S3Config.java
+++ src/main/java/com/saerok/showing/api/global/config/S3Config.java
@@ -11,10 +11,10 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 
-    @Value("${S3_ACCESS_KEY}")
+    @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
 
-    @Value("${S3_SECRET_KEY}")
+    @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
 
     @Value("${cloud.aws.region.static}")
