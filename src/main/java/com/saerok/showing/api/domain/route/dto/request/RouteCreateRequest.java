3d972dcbc96c14e2aeaa9fbbbdf742e8757f7822
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-07-08 15:32:41 +0900

    refactor: 여행코스 인원수 제한 로직 추가

--- src/main/java/com/saerok/showing/api/domain/route/dto/request/RouteCreateRequest.java
+++ src/main/java/com/saerok/showing/api/domain/route/dto/request/RouteCreateRequest.java
@@ -1,6 +1,8 @@
 package com.saerok.showing.api.domain.route.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import lombok.Getter;
@@ -15,14 +17,16 @@ public class RouteCreateRequest {
     private String title;
 
     @NotNull
-    @Schema(description = "여행 시작일(yyyy-MM-dd)", example = "2025-05-21")
+    @Schema(description = "여행 시작일(yyyy-MM-dd)", example = "2025-07-08")
     private LocalDate startDate;
 
     @NotNull
-    @Schema(description = "여행 종료일(yyyy-MM-dd)", example = "2025-05-21")
+    @Schema(description = "여행 종료일(yyyy-MM-dd)", example = "2025-07-08")
     private LocalDate endDate;
 
+    @Min(1)
+    @Max(100)
     @NotNull
-    @Schema(description = "인원수", example = "2")
+    @Schema(description = "인원수, 1~100의 값을 가질 수 있습니다.", example = "15")
     private int peopleCount;
 }
