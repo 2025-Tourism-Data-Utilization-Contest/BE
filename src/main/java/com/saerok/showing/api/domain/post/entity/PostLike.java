c027c51a11bd82f47f21f2e72c697dfccaf0c057
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-07-11 13:24:00 +0900

    feat: 좋아요 엔티티 구현

---
+++ src/main/java/com/saerok/showing/api/domain/post/entity/PostLike.java
@@ -0,0 +1,38 @@
+package com.saerok.showing.api.domain.post.entity;
+
+import com.saerok.showing.api.domain.member.entity.Member;
+import com.saerok.showing.api.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "post_like")
+public class PostLike extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+}
