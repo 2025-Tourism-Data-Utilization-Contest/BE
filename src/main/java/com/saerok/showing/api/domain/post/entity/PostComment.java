1afdfc99a7b566e950734ea5c0c651c563b07101
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-07-11 13:23:47 +0900

    feat: 댓글 엔티티 구현

---
+++ src/main/java/com/saerok/showing/api/domain/post/entity/PostComment.java
@@ -0,0 +1,46 @@
+package com.saerok.showing.api.domain.post.entity;
+
+import com.saerok.showing.api.domain.member.entity.Member;
+import com.saerok.showing.api.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "post_comment")
+public class PostComment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Column(name = "comment", nullable = false, length = 1000)
+    private String comment;
+
+
+}
