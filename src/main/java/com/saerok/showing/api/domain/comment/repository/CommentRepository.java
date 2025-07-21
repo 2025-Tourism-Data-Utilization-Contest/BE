3016b852d5b01a6c2376bc98979b8d36a9f5555a
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-07-21 16:52:31 +0900

    feat: 댓글 조회 로직 구현

---
+++ src/main/java/com/saerok/showing/api/domain/comment/repository/CommentRepository.java
@@ -0,0 +1,13 @@
+package com.saerok.showing.api.domain.comment.repository;
+
+import com.saerok.showing.api.domain.comment.entity.Comment;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findAllByPostId(Long postId);
+
+    Optional<Comment> findByIdAndPostId(Long commentId, Long postId);
+}
