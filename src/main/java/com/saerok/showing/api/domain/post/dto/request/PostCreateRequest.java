dd8730492d8dbe0cbb81a84d3f8be5f565c9b6a2
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-07-21 16:46:00 +0900

    refactor: 게시글 생성 시 해시태그 및 이미지 URL 필드 추가

--- src/main/java/com/saerok/showing/api/domain/post/dto/request/PostCreateRequest.java
+++ src/main/java/com/saerok/showing/api/domain/post/dto/request/PostCreateRequest.java
@@ -4,6 +4,7 @@ import com.saerok.showing.api.domain.post.entity.PostType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,11 +18,22 @@ public class PostCreateRequest {
     private String title;
 
     @NotNull
-    @Schema(description = "게시글 타입", example = "THEME")
+    @Schema(description = "게시글 타입", example = "NORMAL")
     private PostType postType;
 
     @NotNull
     @Size(max = 1000, message = "게시글 내용은 최대 1000자까지 입력할 수 있습니다.")
     @Schema(description = "게시글 내용", example = "순천만 습지 처음 와봤는데 너무 재밌었어요~!")
-    private String comment;
+    private String content;
+
+    @NotNull
+    @Schema(
+        description = "게시글 이미지 URL 리스트",
+        example = "[\"https://bucket.s3.ap-northeast-2.amazonaws.com/post/sample.png\"]"
+    )
+    private List<String> imageUrls;
+
+    @NotNull
+    @Schema(description = "해시태그 목록", example = "[\"#순천만습지\", \"#두루미\"]")
+    private List<String> hashtags;
 }
