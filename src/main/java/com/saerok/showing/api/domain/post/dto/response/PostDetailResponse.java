bc0a05d38d21380e7071b53d36af7c3cf2467ea4
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-07-21 16:46:39 +0900

    refactor: 게시글 응답 DTO 이미지 리스트 필드 추가

--- src/main/java/com/saerok/showing/api/domain/post/dto/response/PostDetailResponse.java
+++ src/main/java/com/saerok/showing/api/domain/post/dto/response/PostDetailResponse.java
@@ -2,7 +2,10 @@ package com.saerok.showing.api.domain.post.dto.response;
 
 import com.saerok.showing.api.domain.post.entity.Post;
 import com.saerok.showing.api.domain.post.entity.PostType;
+import com.saerok.showing.api.global.file.dto.ExternalFileResponse;
+import com.saerok.showing.api.global.file.entity.UploadedFile;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -14,23 +17,36 @@ public class PostDetailResponse {
 
     private String writer;
 
+    private String writerProfileImage;
+
     private String title;
 
-    private String comment;
+    private String content;
+
+    private List<ExternalFileResponse> postImages;
 
     private PostType postType;
+
+    private int likeCount;
+
+    private int commentCount;
 
     private LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
 
-    public static PostDetailResponse toDto(Post post) {
+    public static PostDetailResponse toDto(Post post, int commentCount) {
+        List<UploadedFile> files = post.getPostImages();
         return PostDetailResponse.builder()
             .id(post.getId())
             .writer(post.getMember().getName())
+            .writerProfileImage(post.getMember().getProfileImage())
             .title(post.getTitle())
-            .comment(post.getComment())
+            .content(post.getContent())
+            .postImages(ExternalFileResponse.toListDto(files))
             .postType(post.getPostType())
+            .likeCount(post.getLikeCount())
+            .commentCount(commentCount)
             .createdAt(post.getCreatedAt())
             .updatedAt(post.getUpdatedAt())
             .build();
