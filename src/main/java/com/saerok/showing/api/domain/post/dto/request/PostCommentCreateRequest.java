bdf270c3fac232f6f3970f1ba307bde645032d1f
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-07-21 16:08:35 +0900

    refactor: 댓글 생성 DTO 추가

--- src/main/java/com/saerok/showing/api/domain/post/dto/request/PostCommentCreateRequest.java
+++ src/main/java/com/saerok/showing/api/domain/post/dto/request/PostCommentCreateRequest.java
@@ -1,5 +1,7 @@
 package com.saerok.showing.api.domain.post.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,4 +9,7 @@ import lombok.Setter;
 @Setter
 public class PostCommentCreateRequest {
 
+    @NotNull
+    @Schema(description = "댓글", example = "이 사진 정말 예쁘네요~! 어디인가요?")
+    private String comment;
 }
