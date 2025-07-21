f06020014b4ff0dd623b394b33b7a1ba1847a5cb
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-07-21 16:44:57 +0900

    refactor: 게시글 요약 DTO 구현

--- src/main/java/com/saerok/showing/api/domain/post/dto/response/PostSummaryResponse.java
+++ src/main/java/com/saerok/showing/api/domain/post/dto/response/PostSummaryResponse.java
@@ -3,7 +3,10 @@ package com.saerok.showing.api.domain.post.dto.response;
 import com.saerok.showing.api.domain.member.entity.Member;
 import com.saerok.showing.api.domain.post.entity.Post;
 import com.saerok.showing.api.domain.post.entity.PostType;
+import com.saerok.showing.api.global.file.dto.ExternalFileResponse;
+import com.saerok.showing.api.global.file.entity.UploadedFile;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -19,18 +22,28 @@ public class PostSummaryResponse {
 
     private String writerProfileImage;
 
+    private String content;
+
+    private List<ExternalFileResponse> postImages;
+
     private PostType postType;
 
-    private LocalDateTime createdAt;
+    private int likeCount;
 
-    public static PostSummaryResponse toDto(Post post) {
+    private int commentCount;
+
+    public static PostSummaryResponse toDto(Post post, int commentCount) {
+        List<UploadedFile> files = post.getPostImages();
         return PostSummaryResponse.builder()
             .id(post.getId())
             .title(post.getTitle())
             .writer(post.getMember().getName())
             .writerProfileImage(post.getMember().getProfileImage())
+            .content(post.getContent())
+            .postImages(ExternalFileResponse.toListDto(files))
             .postType(post.getPostType())
-            .createdAt(post.getCreatedAt())
+            .likeCount(post.getLikeCount())
+            .commentCount(commentCount)
             .build();
     }
 }
