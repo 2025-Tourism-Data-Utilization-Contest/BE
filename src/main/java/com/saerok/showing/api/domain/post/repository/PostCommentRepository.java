2fd0bdbf52fcaabe43293d139506d90078b9a9fd
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-07-21 16:09:23 +0900

    refactor: 댓글 조회 로직 구현

--- src/main/java/com/saerok/showing/api/domain/post/repository/PostCommentRepository.java
+++ src/main/java/com/saerok/showing/api/domain/post/repository/PostCommentRepository.java
@@ -1,11 +1,13 @@
 package com.saerok.showing.api.domain.post.repository;
 
-import com.saerok.showing.api.domain.post.entity.Post;
 import com.saerok.showing.api.domain.post.entity.PostComment;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostCommentRepository extends JpaRepository<PostComment, Long> {
 
-    List<PostComment> findByPostOrderByCreatedAtDesc(Post post);
+    List<PostComment> findAllByPostId(Long postId);
+
+    Optional<PostComment> findByIdAndPostId(Long commentId, Long postId);
 }
