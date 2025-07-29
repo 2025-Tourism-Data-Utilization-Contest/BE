bccf5e99c9a257418c8493a65081cc3d78e8c30e
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-07-29 20:49:30 +0900

    refactor: 여행 경로 enum 추가

--- src/main/java/com/saerok/showing/api/domain/like/entity/LikeTargetType.java
+++ src/main/java/com/saerok/showing/api/domain/like/entity/LikeTargetType.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum LikeTargetType {
 
     POST("POST", "게시글"),
-    COMMENT("COMMENT", "댓글");
+    COMMENT("COMMENT", "댓글"),
+    ROUTE("ROUTE", "여행 경로");
 
     private final String key;
     private final String name;
