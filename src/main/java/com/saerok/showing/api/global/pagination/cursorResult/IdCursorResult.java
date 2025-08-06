2cb838323d253cf6c7ea59c2af28e925d8fd121c
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-08-06 16:13:19 +0900

    feat: ID 기반 커서 결과 반환 객체 IdCursorResult 추가

--- src/main/java/com/saerok/showing/api/global/pagination/cursorResult/IdCursorResult.java
+++ src/main/java/com/saerok/showing/api/global/pagination/cursorResult/IdCursorResult.java
@@ -1,28 +1,34 @@
 package com.saerok.showing.api.global.pagination.cursorResult;
 
+import com.saerok.showing.api.global.pagination.cursor.IdCursor;
 import com.saerok.showing.api.global.pagination.provider.IdProvider;
-import java.util.List;
-import lombok.Getter;
 
-@Getter
+import java.util.List;
+
 public record IdCursorResult<T extends IdProvider>(
     List<T> values,
     boolean hasPrevious,
     boolean hasNext
-) {
-    public static <T extends IdProvider> IdCursorResult<T> of(List<T> values, Long cursor, int limit) {
-        boolean hasPrevious = checkFirstPageById(cursor, values);
+) implements CursorResult<T> {
 
+    public static <T extends IdProvider> IdCursorResult<T> of(
+        List<T> values,
+        IdCursor cursor,
+        int limit
+    ) {
+        boolean hasPrevious = checkFirstPageById(cursor, values);
         boolean hasNext = values.size() > limit;
         List<T> trimmedValues = hasNext ? values.subList(0, limit) : values;
-
         return new IdCursorResult<>(trimmedValues, hasPrevious, hasNext);
     }
 
-    private static <T extends IdProvider> boolean checkFirstPageById(Long cursor, List<T> values) {
-        return cursor != null
-            && cursor > 0
-            && !values.isEmpty()
-            && values.getFirst().getId() > cursor;
+    private static <T extends IdProvider> boolean checkFirstPageById(
+        IdCursor cursor,
+        List<T> values
+    ) {
+        if (cursor == null || values.isEmpty()) {
+            return false;
+        }
+        return values.getFirst().getId() > cursor.id();
     }
 }
