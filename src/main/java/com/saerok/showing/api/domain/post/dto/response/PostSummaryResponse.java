aa449496500c43e148bbb57683e39e0ff5fb3f1b
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-08-06 16:17:43 +0900

    refactor: 좋아요 및 생성일자 오버라이드

--- src/main/java/com/saerok/showing/api/domain/post/dto/response/PostSummaryResponse.java
+++ src/main/java/com/saerok/showing/api/domain/post/dto/response/PostSummaryResponse.java
@@ -1,20 +1,19 @@
 package com.saerok.showing.api.domain.post.dto.response;
 
-import com.saerok.showing.api.domain.member.entity.Member;
 import com.saerok.showing.api.domain.post.entity.Post;
 import com.saerok.showing.api.domain.post.entity.PostType;
 import com.saerok.showing.api.global.file.dto.ExternalFileResponse;
 import com.saerok.showing.api.global.file.entity.UploadedFile;
 import com.saerok.showing.api.global.pagination.provider.CreatedAtProvider;
+import com.saerok.showing.api.global.pagination.provider.PopularProvider;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
-public class PostSummaryResponse implements CreatedAtProvider {
+public class PostSummaryResponse implements CreatedAtProvider, PopularProvider {
 
     private Long id;
 
@@ -35,6 +34,16 @@ public class PostSummaryResponse implements CreatedAtProvider {
     private int commentCount;
 
     private LocalDateTime createdAt;
+
+    @Override
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    @Override
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 
     public static PostSummaryResponse toDto(Post post, int commentCount) {
         Optional<UploadedFile> firstFile = post.getPostImages().stream().findFirst();
