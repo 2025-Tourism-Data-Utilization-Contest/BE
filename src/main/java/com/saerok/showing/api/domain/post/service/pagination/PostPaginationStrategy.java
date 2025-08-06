df2bccd31fca835cb7aeb28c69eb945c2f6220a8
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-08-06 16:15:19 +0900

    feat: 페이지네이션 전략 인터페이스 PostPaginationStrategy 정의

---
+++ src/main/java/com/saerok/showing/api/domain/post/service/pagination/PostPaginationStrategy.java
@@ -0,0 +1,25 @@
+package com.saerok.showing.api.domain.post.service.pagination;
+
+import com.saerok.showing.api.domain.comment.service.CommentCountProvider;
+import com.saerok.showing.api.domain.post.dto.response.PostSummaryResponse;
+import com.saerok.showing.api.domain.post.entity.Post;
+import com.saerok.showing.api.domain.post.entity.PostSortType;
+import com.saerok.showing.api.domain.post.entity.PostType;
+import com.saerok.showing.api.global.pagination.cursorResult.CursorResult;
+import java.util.List;
+
+public interface PostPaginationStrategy {
+
+    PostSortType getSortType();
+
+    Object parseCursor(String rawCursor);
+
+    List<Post> paginate(PostType postType, Object cursor, int limit);
+
+    CursorResult<PostSummaryResponse> getCursorResult(
+        PostType postType,
+        String rawCursor,
+        int limit,
+        CommentCountProvider commentCountProvider
+    );
+}
