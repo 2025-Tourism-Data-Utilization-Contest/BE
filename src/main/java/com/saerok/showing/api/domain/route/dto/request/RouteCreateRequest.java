06661d8e3ae3791f3f04ec938d2b046c5d49b0c6
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-08-27 15:59:39 +0900

    refactor: 여행코스 생성 DTO 이름 검증 로직 추가

--- src/main/java/com/saerok/showing/api/domain/route/dto/request/RouteCreateRequest.java
+++ src/main/java/com/saerok/showing/api/domain/route/dto/request/RouteCreateRequest.java
@@ -12,21 +12,19 @@ import lombok.Setter;
 @Setter
 public class RouteCreateRequest {
 
-    @NotNull
-    @Schema(description = "내 여행코스 이름", example = "5학년 3반 나들이 여행코스")
+    @Min(1) @Max(50) @NotNull
+    @Schema(description = "내 여행코스 이름, 여행코스는 1자 이상 50자 이하여야 합니다.", example = "5학년 3반 나들이 여행코스")
     private String title;
 
     @NotNull
-    @Schema(description = "여행 시작일(yyyy-MM-dd)", example = "2025-07-08")
+    @Schema(description = "여행 시작일(yyyy-MM-dd)", example = "2025-09-18")
     private LocalDate startDate;
 
     @NotNull
-    @Schema(description = "여행 종료일(yyyy-MM-dd)", example = "2025-07-08")
+    @Schema(description = "여행 종료일(yyyy-MM-dd)", example = "2025-09-18")
     private LocalDate endDate;
 
-    @Min(1)
-    @Max(100)
-    @NotNull
+    @Min(1) @Max(100) @NotNull
     @Schema(description = "인원수, 1~100의 값을 가질 수 있습니다.", example = "15")
     private int peopleCount;
 }
