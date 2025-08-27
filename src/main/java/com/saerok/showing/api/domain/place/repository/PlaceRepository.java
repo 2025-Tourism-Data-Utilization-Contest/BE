54fcfb2e8ff8da773e2a337ed8b49a1f0281888d
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-08-27 14:34:57 +0900

    refactor: 이름으로 장소 조회 로직 추가

--- src/main/java/com/saerok/showing/api/domain/place/repository/PlaceRepository.java
+++ src/main/java/com/saerok/showing/api/domain/place/repository/PlaceRepository.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PlaceRepository extends JpaRepository<Place, Long> {
 
+    Place findByTitleContainingIgnoreCase(String title);
+
     @Query("""
             SELECT p FROM Place p
             WHERE p.locationY BETWEEN :minLat AND :maxLat
