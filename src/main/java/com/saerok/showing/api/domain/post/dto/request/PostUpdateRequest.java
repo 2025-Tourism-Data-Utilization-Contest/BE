5ec062baea7883885dad697a85c0618ccbfab5eb
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-08-29 17:58:40 +0900

    refactor: 게시글 생성 및 수정 시 가시성 필드 추가

--- src/main/java/com/saerok/showing/api/domain/post/dto/request/PostUpdateRequest.java
+++ src/main/java/com/saerok/showing/api/domain/post/dto/request/PostUpdateRequest.java
@@ -1,8 +1,10 @@
 package com.saerok.showing.api.domain.post.dto.request;
 
+import com.saerok.showing.api.domain.post.entity.PostVisibility;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,7 +18,15 @@ public class PostUpdateRequest {
     private String title;
 
     @NotNull
+    @Schema(description = "게시글 가시성", example = "VISIBLE_ALL")
+    private PostVisibility visibility;
+
+    @NotNull
     @Size(max = 1000, message = "게시글 내용은 최대 1000자까지 입력할 수 있습니다.")
     @Schema(description = "게시글 내용", example = "순천만 습지 처음 와봤는데 너무 재밌었어요~!!!!!!")
     private String content;
+
+    @NotNull
+    @Schema(description = "해시태그 목록", example = "[\"#순천만습지\", \"#두루미\"]")
+    private List<String> hashtags;
 }
