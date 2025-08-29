1c14eed39d1377ed0ff39eb705f172c55f92fc2e
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-08-29 15:30:33 +0900

    refactor: 새 조회 로직 리팩토링

--- src/main/java/com/saerok/showing/api/domain/birdTheme/service/BirdThemeService.java
+++ src/main/java/com/saerok/showing/api/domain/birdTheme/service/BirdThemeService.java
@@ -1,7 +1,6 @@
 package com.saerok.showing.api.domain.birdTheme.service;
 
-import com.saerok.showing.api.domain.bird.dto.response.BirdSummaryResponse;
-import com.saerok.showing.api.domain.bird.entity.Bird;
+import com.saerok.showing.api.domain.birdTheme.dto.response.BirdThemeResponse;
 import com.saerok.showing.api.domain.birdTheme.respository.BirdThemeRepository;
 import com.saerok.showing.api.domain.theme.dto.response.ThemeSummaryResponse;
 import com.saerok.showing.api.domain.theme.entity.Theme;
@@ -18,11 +17,10 @@ public class BirdThemeService {
     private final BirdThemeRepository birdThemeRepository;
 
     @Transactional(readOnly = true)
-    public List<BirdSummaryResponse> getBirdNames(Long themeId) {
-        List<Bird> birds = birdThemeRepository.findBirdsByThemeId(themeId);
-        return birds.stream()
-            .map(BirdSummaryResponse::toDto)
-            .collect(Collectors.toList());
+    public List<BirdThemeResponse> getBirdsByTheme(Long themeId) {
+        return birdThemeRepository.findByThemeId(themeId).stream()
+            .map(BirdThemeResponse::from)
+            .toList();
     }
 
     @Transactional(readOnly = true)
