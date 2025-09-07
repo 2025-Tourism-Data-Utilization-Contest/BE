08d3fd16771044884772e89bd2a2101bd2b0dd80
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-09-07 22:15:26 +0900

    refactor: 좋아요 조회 로직 구현

--- src/main/java/com/saerok/showing/api/domain/like/repository/LikeRepository.java
+++ src/main/java/com/saerok/showing/api/domain/like/repository/LikeRepository.java
@@ -5,6 +5,7 @@ import com.saerok.showing.api.domain.like.entity.LikeTargetType;
 import com.saerok.showing.api.domain.member.entity.Member;
 import com.saerok.showing.api.domain.like.entity.Like;
 import com.saerok.showing.api.domain.post.entity.Post;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -13,6 +14,8 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
     Optional<Like> findByMemberAndPost(Member member, Post post);
 
     Optional<Like> findByMemberAndComment(Member member, Comment comment);
+
+    List<Like> findByMemberAndTargetType(Member member, LikeTargetType targetType);
 
     Optional<Like> findByMemberAndTargetIdAndTargetType(Member member, Long targetId, LikeTargetType targetType);
 }
