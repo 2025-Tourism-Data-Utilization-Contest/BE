cd0163595f68819ec0d957cf859f48a9a25e4d99
김민상 <202014889@kyonggi.ac.kr>
2025-09-18 09:02:39 +0900

    hotfix: 누락된 import문 추가

--- src/main/java/com/saerok/showing/api/global/auth/controller/GuestAuthController.java
+++ src/main/java/com/saerok/showing/api/global/auth/controller/GuestAuthController.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
